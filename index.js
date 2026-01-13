d507786501f492a09b3c510de3450d3d84c0567c
Nathanmcl05 <Nathanmcloughlin2021@gmail.com>
2026-01-13 11:22:52 +0000

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require("express")
+var app = express()
+
+app.set('view engine', 'ejs') // set template engine
+
+app.get('/', (req, res) => {
+
+    res.render('home')
+    
+})
+
+
+app.get('/about', (req, res) => {
+
+    res.render('about')
+    
+})
+
+//********************* Code to here *************/
+
+app.listen(process.env.Port || 3000, process.env.IP || "0.0.0.0" , function(){
+    console.log("New Full Demo is Live")
+});
