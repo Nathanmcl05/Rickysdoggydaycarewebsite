72ec513db9ebc2c38d1e66d3431ea1625886e08a
Nathanmcl05 <Nathanmcloughlin2021@gmail.com>
2026-01-14 12:42:52 +0000

    commit

--- index.js
+++ index.js
@@ -7,9 +7,6 @@ app.get('/', (req, res) => {
     res.render('home')
 })
 
-app.get('/about', (req, res) => {
-    res.render('about')
-})
 
 app.get('/booking', (req, res) => {
     res.render('booking')
